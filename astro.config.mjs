54320e6d9d37618858250c7d7b0ef3936e6fa6bf
matheus martins <mathews_2010@outlook.com>
2023-06-22 21:21:21 -0300

    chore: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,23 @@ import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
 import WindiCSS from 'vite-plugin-windicss';
 import Icons from 'unplugin-icons';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [VitePWA({
-			registerType: 'autoUpdate',
-			manifest: { 
-              short_name: 'Matheus Martins',
-			  name: 'Matheus Martins | Blog',
-			  start_url: '/'
-			}
-		}), WindiCSS(), Icons]
-	}
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  vite: {
+    plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Matheus Martins',
+        name: 'Matheus Martins | Blog',
+        start_url: '/'
+      }
+    }), WindiCSS(), Icons]
+  }
 });
