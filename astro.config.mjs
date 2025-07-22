43e55d4eb6741592b6dc5f1da06fadb1b456bd35
mmartinsoliv <mathews_2010@outlook.com>
2025-07-22 19:22:37 -0300

    refactor: update Astro configuration and dependencies, migrate to pnpm, and remove unused components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,15 @@
 import { defineConfig } from 'astro/config';
-import { VitePWA } from 'vite-plugin-pwa';
-import WindiCSS from 'vite-plugin-windicss';
-import Icons from 'unplugin-icons';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
   vite: {
-    plugins: [VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        short_name: 'Matheus Martins',
-        name: 'Matheus Martins | Blog',
-        start_url: '/'
-      }
-    }), WindiCSS(), Icons]
-  }
+    plugins: [tailwindcss()],
+  },
 });
